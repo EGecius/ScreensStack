fda4fc1a3736f878203079f2259122124a3a1b75
update gradle <egidijusgecius@Egidijuss-MBP.default>
2017-11-25 09:28:42 +0000

    simplify use of Application.ActivityLifecycleCallbacks

--- library/src/main/java/com/egecius/mylibrary/ScreensStack.java
+++ library/src/main/java/com/egecius/mylibrary/ScreensStack.java
@@ -4,12 +4,16 @@ import android.app.Activity;
 import android.app.Application;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 import android.util.Log;
 
 public class ScreensStack {
 
-    private Application.ActivityLifecycleCallbacks mCallback = new Application.ActivityLifecycleCallbacks() {
+    public void init(Application application) {
+        application.registerActivityLifecycleCallbacks(
+                new MyActivityLifecycleCallbacks());
+    }
+
+    private class MyActivityLifecycleCallbacks extends SimpleActivityLifecycleCallbacks {
         @Override
         public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
             registerFragmentCallbacks(activity);
@@ -21,37 +25,9 @@ public class ScreensStack {
         }
 
         @Override
-        public void onActivityResumed(Activity activity) {
-
-        }
-
-        @Override
-        public void onActivityPaused(Activity activity) {
-
-        }
-
-        @Override
         public void onActivityStopped(Activity activity) {
             Log.w("Eg:ScreensStack:41", "onActivityStopped activity " + activity);
         }
-
-        @Override
-        public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
-
-        }
-
-        @Override
-        public void onActivityDestroyed(Activity activity) {
-
-        }
-    };
-
-    private FragmentManager.FragmentLifecycleCallbacks
-            mFragmentLifecycleCallbacks = new MyFragmentLifecycleCallbacks();
-
-    public void init(Application application) {
-        application.registerActivityLifecycleCallbacks(
-                mCallback);
     }
 
     private void registerFragmentCallbacks(Activity activity) {
@@ -60,8 +36,6 @@ public class ScreensStack {
         }
 
         ((FragmentActivity) activity).getSupportFragmentManager()
-                .registerFragmentLifecycleCallbacks(
-                        mFragmentLifecycleCallbacks, false);
+                .registerFragmentLifecycleCallbacks(new MyFragmentLifecycleCallbacks(), false);
     }
-
 }
